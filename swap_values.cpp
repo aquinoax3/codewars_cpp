f6286e61549bf805edf9f4b8500349268ce8e8ff
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-01-04 23:52:15 -0500

    Cleaned up code

--- swap_values.cpp
+++ swap_values.cpp
@@ -8,6 +8,5 @@ std::pair<int, int> swap_values(std::pair<int, int> values) {
     int temp = values.first;
     values.first = values.second;
     values.second = temp;
-  
     return {values.first, values.second};
 }
