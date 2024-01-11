e01b9dcec308682f4a7fda94ad9dbc5b1f9c9156
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-01-10 23:58:18 -0500

    Created basic_mathematical_equation.cpp

---
+++ basic_mathematical_equation.cpp
@@ -0,0 +1,17 @@
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+int basicOp(char op, int val1, int val2) {
+    if (op == '+') return val1 + val2;
+    if (op == '-') return val1 - val2;
+    if (op == '*') return val1 * val2;
+    if (op == '/' && val2 != 0) return val1 / val2;
+}
