04392cdd0428e4d764c178ae1d7300ebfe10586d
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-01-30 23:51:29 -0500

    Created terminal_game_combat_funtion.cpp

---
+++ terminal_game_combat_funtion.cpp
@@ -0,0 +1,6 @@
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+int combat(int health, int damage){
+    if(health < damage) return 0;
+    return health - damage;
+}
