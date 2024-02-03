2ec4966eca6d44fd31119790cbd54d5e523981c8
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-02-03 12:28:18 -0500

    Created fake_binary.cpp

---
+++ fake_binary.cpp
@@ -0,0 +1,21 @@
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+#include <iostream>
+#include <string>
+
+std::string fakeBin(std::string str){
+    std::string newBin = "";
+    //   for (int i = 0; i < str.size(); i++) {
+    //    if(int (str[i]) < 5) {
+    //      str[i] = "0";
+    //      newBin += str[i]
+    //    } else {
+    //      str[i] = "1";
+    //      newBin += str[i]
+    //    }
+    //   }
+    std::cout << int (str[1]) << std::endl;
+//   return newBin;
+}
