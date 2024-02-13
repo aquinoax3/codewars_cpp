4b21d0dd44874218c5a39daa5d86ac9193364158
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-02-13 00:01:47 -0500

    Created unfinished_loop.cpp

---
+++ unfinished_loop.cpp
@@ -0,0 +1,12 @@
+// Unfinished Loop - Bug Fixing #1
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+std::vector<int> createVector(const int n) {
+	std::vector<int> res;
+
+	for (int i = 1; i <= n; i++){
+		res.push_back(i);
+	}
+
+	return res;
+}
