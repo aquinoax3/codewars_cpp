ee3a57fcbd7f1e89817e02b9671a6b27a316c205
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-02-13 00:02:15 -0500

    Include vector library

--- unfinished_loop.cpp
+++ unfinished_loop.cpp
@@ -1,6 +1,8 @@
 // Unfinished Loop - Bug Fixing #1
 // Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
 
+#include <vector>
+
 std::vector<int> createVector(const int n) {
 	std::vector<int> res;
 
