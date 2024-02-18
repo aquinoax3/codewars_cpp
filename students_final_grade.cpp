c174ea547fbd9e8f8911c8aa8991be88f82f1204
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-02-18 18:06:02 -0500

    included logic for function

--- students_final_grade.cpp
+++ students_final_grade.cpp
@@ -24,5 +24,7 @@
 
 
 int final_grade(int exam, int projects){
-   return
+    return exam > 90 || projects > 10 ? 100 : 
+    exam > 75 && projects >= 5 ? 90 :
+    exam > 50 && projects >= 2 ? 75 : 0;
 }
