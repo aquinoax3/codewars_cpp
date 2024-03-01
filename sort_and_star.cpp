4bd4303c6fc2a3ce3151c62e344476b12a394f73
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-02-29 23:55:24 -0500

    Created sort_and_star.cpp

---
+++ sort_and_star.cpp
@@ -0,0 +1,15 @@
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+
+
+#include <vector>
+#include <string>
+
+std::string twoSort(std::vector<std::string> s)
+{
+    return "";
+}
