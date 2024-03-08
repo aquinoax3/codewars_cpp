ebc4ad09ef072ac754b4e9aad9698fb059a16126
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-03-08 00:05:48 -0500

    Created opposite_number.cpp

---
+++ opposite_number.cpp
@@ -0,0 +1,13 @@
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+int opposite(int number) 
+{
+  return number * -1;
+}
