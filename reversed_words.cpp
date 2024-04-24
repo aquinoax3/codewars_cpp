139f637d8615c682395edb4a146e5c230899a71c
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-04-24 00:42:18 -0400

    Added logic to function

--- reversed_words.cpp
+++ reversed_words.cpp
@@ -5,3 +5,26 @@
 // Example(Input --> Output):
 
 // "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+#include <string>
+#include <sstream>
+#include <vector>
+
+std::string reverse_words(const std::string &str) {
+    std::vector<std::string> newArr;
+    std::stringstream ss(str);
+    std::string word;
+    std::string result;
+
+    while (ss >> word)
+    {
+        newArr.push_back(word);
+    }
+
+    for (int i = newArr.size() - 1; i >= 0; i--)
+    {
+        result += newArr[i] + " ";
+    }
+    result.pop_back();
+    return result;
+}
