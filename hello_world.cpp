26e4206b1741d90a45cfa675ad7f748e4c591692
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-11-08 21:50:11 -0500

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+
+#include <string>
+
+// Write a function "greet" that returns "hello world!" of type std::string
+
+std::string greet() {
+  return "hello world!";
+}
