9fd1148e806d1a05dc2411bc5ba96106fa14811f
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2025-07-04 16:10:40 -0400

    Create lario_and_muigi_pipe_problems.cpp

---
+++ lario_and_muigi_pipe_problems.cpp
@@ -0,0 +1,27 @@
+// Issue
+// Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+// The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+// The pipes are correct when each pipe after the first is 1 more than the previous one.
+
+// Task
+// Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+// Example
+// Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+
+
+#include <vector>
+#include <iostream>
+
+std::vector<int> pipe_fix(const std::vector<int>& nums) {
+  std::vector<int> result;
+  
+  for (int i = nums[0] ; i <= nums[nums.size() -1]; i++) {
+    result.push_back(i);
+  }
+  
+  return result;
+}
