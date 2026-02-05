b1c40e6ecd9b29bb3c8c2a7a1c92c8b687684b41
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2026-02-04 23:53:52 -0500

    Create bin_to_decimal.cpp

---
+++ bin_to_decimal.cpp
@@ -0,0 +1,9 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+
+#include <string>
+
+unsigned long binToDec(const std::string &str) {
+  return 0;
+}
